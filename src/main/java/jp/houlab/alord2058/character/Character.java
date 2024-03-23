5266a947b85906aa772933711012a4f6509455d6
Aload2058 <aload2058@gmail.com>
2024-03-23 13:01:24 +0900

    20240320

--- src/main/java/jp/houlab/alord2058/character/Character.java
+++ src/main/java/jp/houlab/alord2058/character/Character.java
@@ -1,6 +1,6 @@
 package jp.houlab.alord2058.character;
 
-import jp.houlab.alord2058.character.kazenomatasaburou.KazenoMatasaburou;
+import jp.houlab.alord2058.character.kazenomatasaburou.PassiveSkill;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Character extends JavaPlugin {
@@ -9,7 +9,7 @@ public final class Character extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("on");
-        getServer().getPluginManager().registerEvents(new KazenoMatasaburou(this), this);
+        getServer().getPluginManager().registerEvents(new PassiveSkill(this), this);
     }
 
     @Override
---
+++ src/main/java/jp/houlab/alord2058/character/kazenomatasaburou/PassiveSkill.java
@@ -0,0 +1,91 @@
+package jp.houlab.alord2058.character.kazenomatasaburou;
+
+import jp.houlab.alord2058.character.Character;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityToggleGlideEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.scheduler.BukkitRunnable;
+import java.util.Set;
+
+public class PassiveSkill implements Listener {
+
+    //Javaplugin constructor
+    private final Character javaplugin;
+    public PassiveSkill(Character javaplugin) {
+        this.javaplugin = javaplugin;
+    }
+
+    //Count until recharge
+    private int count = 0;
+
+    //Passive Skill
+    @EventHandler
+    public void onElytraGride(EntityToggleGlideEvent event) {
+        Player player = (Player) event.getEntity();
+        int maxEnergy = this.javaplugin.getConfig().getInt("kazenomatasaburou.maxEnergy");
+        int rechargeEnergy = this.javaplugin.getConfig().getInt("kazenomatasaburou.rechargeEnergy");
+        int rechargeEnergyCT = this.javaplugin.getConfig().getInt("kazenomatasaburou.rechargeEnergyCT");
+        int minEnergy = this.javaplugin.getConfig().getInt("kazenomatasaburou.minEnergy");
+        Set<String> tag = player.getScoreboardTags();
+
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (tag.contains("kazenomatasaburou")) {
+                    int playerCurrentEnergy = player.getLevel();
+                    boolean isGliding = player.isGliding();
+
+                    if (playerCurrentEnergy > 0 && isGliding) {
+                        player.setExp((float) playerCurrentEnergy / maxEnergy);
+                        player.setLevel((playerCurrentEnergy - 1));
+                        count = 0;
+
+                    } else if (playerCurrentEnergy == 0 && isGliding) {
+                        player.damageItemStack(EquipmentSlot.CHEST, -432);
+                        player.damageItemStack(EquipmentSlot.CHEST, 431);
+
+                    } else if (!isGliding) {
+                        this.cancel();
+                    }
+                }
+            }
+        }.runTaskTimer(javaplugin,0,1);
+
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (tag.contains("kazenomatasaburou")) {
+                    int playerCurrentEnergy = player.getLevel();
+                    boolean isGliding = player.isGliding();
+
+                    if (count == 40) {
+                        if (playerCurrentEnergy == minEnergy && !isGliding) {
+                            player.damageItemStack(EquipmentSlot.CHEST, -432);
+                            player.setLevel(playerCurrentEnergy + rechargeEnergy);
+
+                        } else if (playerCurrentEnergy <= maxEnergy - 2 && !isGliding){
+                            player.setLevel(playerCurrentEnergy + rechargeEnergy );
+                            player.setExp((float) playerCurrentEnergy / maxEnergy);
+
+                        } else if (playerCurrentEnergy == (maxEnergy - 1) && !isGliding){
+                            player.setLevel(playerCurrentEnergy + (rechargeEnergy -1) );
+                            player.setExp((float) playerCurrentEnergy / maxEnergy);
+
+                        } else if (playerCurrentEnergy == maxEnergy) {
+                            player.setExp((float) playerCurrentEnergy / maxEnergy);
+                        }
+
+                    } else if (count < rechargeEnergyCT && !isGliding) {
+                        count++;
+
+                    } else if (isGliding) {
+                        this.cancel();
+                    }
+                }
+            }
+        }.runTaskTimer(javaplugin,0,1);
+    }
+}
